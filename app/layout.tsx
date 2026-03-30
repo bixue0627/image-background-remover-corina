2e81e705a988377bd18d7e6f6b253a8449ee1c79
corina <bixue0627@163.com>
2026-03-30 11:55:20 +0800

    fix: correct layout syntax

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,14 @@ export const metadata: Metadata = {
   description: "Image background Remover application",
 };
 
-export default function RootLayout(
-  children: ReactNode>
-) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-   <html lang="en">
-    <body>ãhildren</body>
-  </html>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
